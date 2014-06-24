1e6cf2b58c77cecab35c23b6f343117b291335c1
rfraposa <rich@hortonworks.com>
2014-06-24 06:55:23 -0600

    Added Tez JAR files to HDFS

--- hdp_node/scripts/createHDFSdirectories.sh
+++ hdp_node/scripts/createHDFSdirectories.sh
@@ -39,8 +39,12 @@ fs -chown -R hcat:users /apps/webhcat;
 fs -chmod -R 755 /apps/webhcat;
 
 -- Create Tez folders
-fs -mkdir -p /apps/tez
+fs -mkdir -p /apps/tez/lib
+fs -copyFromLocal /usr/lib/tez/* /apps/tez
 fs -chmod 755 /apps/tez
+fs -chmod 755 /apps/tez/lib/
+fs -chmod 644 /apps/tez/*.jar
+fs -chmod 644 /apps/tez/lib/*.jar
 
 -- Create Oozie folder
 fs -mkdir /user/oozie
