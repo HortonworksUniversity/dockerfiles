1872334172aab1085f257bf474e5ee2ac119adbe
rfraposa <rich@hortonworks.com>
2014-06-24 19:59:10 -0600

    Add bash comment

--- hdp_node/start-scripts/10-install-tez.sh
+++ hdp_node/start-scripts/10-install-tez.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 if [[ ("$NODE_TYPE" == "hiveserver") ]]; then
+	echo "Installing Tez JAR files.."
 	sudo -E -u hdfs hadoop fs -put -f /usr/lib/tez/* /apps/tez
  	sudo -E -u hdfs hadoop fs -put -f /usr/lib/hive/lib/hive-exec.jar /apps/hive/install/
 fi
