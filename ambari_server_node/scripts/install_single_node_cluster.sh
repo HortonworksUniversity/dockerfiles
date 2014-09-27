63db5078fce6889e892202fef065d4bcd9442eef
Michael Bingham <michael.b.b.bingham@gmail.com>
2014-09-26 22:33:02 -0700

    Added minimal Hadoop/Pig/Hive blueprint for Ambari single-node Docker deployment

--- ambari_server_node/scripts/install_single_node_cluster.sh
+++ ambari_server_node/scripts/install_single_node_cluster.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+BLUEPRINT_FILE=$1
+
+: ${BLUEPRINT_FILE:="singlenode.blueprint"}
+BLUEPRINT_NAME="${BLUEPRINT_FILE%.*}"
+
 java -jar /root/scripts/ambari-shell.jar --ambari.host=namenode << END &
-blueprint add --file /root/blueprints/singlenode.blueprint
-cluster build --blueprint singlenode
+blueprint add --file /root/blueprints/$BLUEPRINT_FILE
+cluster build --blueprint $BLUEPRINT_NAME  
 cluster assign --hostGroup master --host namenode
 cluster create --exitOnFinish true
 quit
